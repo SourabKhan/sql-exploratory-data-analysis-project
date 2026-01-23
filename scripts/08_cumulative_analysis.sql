f2305016699d022cf48cdeafca195fb7cecf6db6
Sourab Khan <108218692+SourabKhan@users.noreply.github.com>
2026-01-23 09:44:35 +0530

    Update and rename 8_cumulative_analysis to 08_cumulative_analysis.sql

---
+++ scripts/08_cumulative_analysis.sql
@@ -0,0 +1,39 @@
+-- Cumulative Analysis
+-- To calculate running totals or moving averages for key metrics.
+-- To track performance over time cumulatively.
+-- Useful for growth analysis or identifying long-term trends.
+
+-- Calculate the total sales per month and the running total of sales over time.
+-- Year
+
+SELECT 
+	order_date,
+	total_sales,
+	SUM(total_sales) OVER(ORDER BY order_date) AS running_total_sales,
+	AVG(avg_price) OVER(ORDER BY order_date) AS moving_average_price
+FROM(
+SELECT 
+	DATETRUNC(YEAR, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales,
+	AVG(price) AS avg_price
+FROM gold.fact_sales
+WHERE DATETRUNC(YEAR, order_date) IS NOT NULL
+GROUP BY DATETRUNC(YEAR, order_date)
+)t 
+
+-- Month
+
+SELECT 
+	order_date,
+	total_sales,
+	SUM(total_sales) OVER(PARTITION BY order_date ORDER BY order_date) AS running_total_sales,
+	AVG(avg_price) OVER(PARTITION BY order_date ORDER BY order_date) AS moving_average_price
+FROM(
+SELECT 
+	DATETRUNC(MONTH, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales,
+	AVG(price) AS avg_price
+FROM gold.fact_sales
+WHERE DATETRUNC(MONTH, order_date) IS NOT NULL
+GROUP BY DATETRUNC(MONTH, order_date)
+)t 
