285225fec023e726899730574d8fe4baa73d2f35
Sourab Khan <108218692+SourabKhan@users.noreply.github.com>
2026-02-01 09:26:56 +0530

    Implement part-to-whole analysis for sales categories
    Added a SQL query for part-to-whole analysis of category sales.

--- scripts/11_part_of_whole_analysis.sql
+++ scripts/11_part_of_whole_analysis.sql
@@ -1,1 +1,20 @@
+-- Part-to-Whole Analysis
 
+-- Which categories contribute the most to overall sales?
+WITH category_sales AS (
+SELECT 
+	p.category,
+	SUM(f.sales_amount) AS total_sales
+FROM gold.fact_sales AS f
+LEFT JOIN gold.dim_products AS p
+ON p.product_key = f.product_key
+GROUP BY p.category
+)
+
+SELECT 
+	category,
+	total_sales,
+	SUM(total_sales) OVER() AS overall_sales,
+	ROUND(CAST(total_sales AS FLOAT) / SUM(total_sales) OVER() * 100,2) AS percentage_of_total
+FROM category_sales
+ORDER BY percentage_of_total DESC;
